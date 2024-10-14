f7b816add252dc952b50c70be28c3e09ef4f287e
Yash Rana Kumar <ynk24@ic.ac.uk>
2024-10-14 17:39:25 +0100

    Add catometer functionality

--- src/app.py
+++ src/app.py
@@ -3,13 +3,18 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def homepage():
     return render_template("index.html")
 
 @app.route("/submit", methods=["POST"])
 def submit():
-    input_name = request.form.get("name")
-    input_favourite_num = request.form.get("favouriteNumber")
-    return render_template("hello.html", name=input_name, favouriteNumber=input_favourite_num)
+    number_cats = int(request.form.get("cat_number"))
+    if (number_cats <= 3):
+        return render_template("cat1.html")
+    elif(number_cats <= 6):
+        return render_template("cat2.html")
+    else:
+        return render_template("cat3.html")
+    
 
      
