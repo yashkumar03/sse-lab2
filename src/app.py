6b6e58174a272f6e7e9acbb527a2774dc4da61e3
Yile Huang <yile.huang24@imperial.ac.uk>
2024-10-21 14:22:38 +0100

    formatting

--- src/app.py
+++ src/app.py
@@ -2,19 +2,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def homepage():
     return render_template("index.html")
 
+
 @app.route("/submit", methods=["POST"])
 def submit():
     number_cats = int(request.form.get("cat_number"))
-    if (number_cats <= 3):
+    if number_cats <= 3:
         return render_template("cat1.html")
-    elif(number_cats <= 6):
+    elif number_cats <= 6:
         return render_template("cat2.html")
     else:
         return render_template("cat3.html")
-    
-
-     
