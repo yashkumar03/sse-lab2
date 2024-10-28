ce49a4bb669d1f01a11bf4eab24fe59689175af3
Yash Rana Kumar <ynk24@ic.ac.uk>
2024-10-28 16:31:33 +0000

    final version

--- src/app_test.py
+++ src/app_test.py
@@ -47,7 +47,7 @@ def test_squares_and_cubes():
     assert (
         process_query(
             "Which of the following numbers is both \
-    a square and a cube: 400, 2744, 2445, 4783, 729, 3191, 3439?
+        a square and a cube: 400, 2744, 2445, 4783, 729, 3191, 3439?"
         )
         == "729"
     )
